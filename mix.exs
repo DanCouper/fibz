3f907aed4909435aea083e98f2b2b5e4609798da
DanCouper <danielcouper81@googlemail.com>
2017-11-11 14:13:38 +0000

    Add StreamData and Credo deps

--- mix.exs
+++ mix.exs
@@ -21,8 +21,10 @@ defmodule Fibz.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:stream_data, "~> 0.3", only: :test},
+      # Credo has an issue on Elixir 1.6 - see https://github.com/rrrene/credo/issues/463
+      # pull from master until new fixed version is released.
+      {:credo, github: "rrrene/credo", only: [:dev, :test], runtime: false}
     ]
   end
 end
