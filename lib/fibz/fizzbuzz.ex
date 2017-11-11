62c19ff4f91d5bf1cc011e9efbc7352c90680f6b
DanCouper <danielcouper81@googlemail.com>
2017-11-11 14:22:36 +0000

    Add FizzBuzz `compute` function

--- lib/fibz/fizzbuzz.ex
+++ lib/fibz/fizzbuzz.ex
@@ -4,5 +4,12 @@ defmodule Fibz.Fizzbuzz do
   'tis what it is.
   """
 
-
+  @doc """
+  Given an int, returns the corresponding fizz/buzz/fizzbuzz/int value.
+  """
+  @spec parse_int(integer) :: String.t() | integer
+  def parse_int(i) when rem(i, 15) == 0, do: "fizzbuzz"
+  def parse_int(i) when rem(i, 3) == 0, do: "fizz"
+  def parse_int(i) when rem(i, 5) == 0, do: "buzz"
+  def parse_int(i), do: i
 end
