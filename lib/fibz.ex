9b0da9f621f0666bd0af8a6c55e24381afe6b868
DanCouper <danielcouper81@googlemail.com>
2017-11-11 14:57:34 +0000

    Delegate core functions to root
    Allow the application to be used from the root namespace - i.e. instead of `Fibz.Server.compute(1)` or `Fibz.Server.stop()`, instead just `Fibz.compute(1)` or `Fibz.stop_compute_server()`

--- lib/fibz.ex
+++ lib/fibz.ex
@@ -5,4 +5,34 @@ defmodule Fibz do
   def start(_type, _args) do
     Fibz.Supervisor.start_link()
   end
+
+  @doc ~S"""
+  Given an integer, compute the correct fizzbuzz return value.
+  NOTE will hit the cache, and only actually run the computation
+  if the value is not available.
+
+  ## Example
+
+      iex> Application.start(Fibz, [])
+      ...> Fibz.compute(1)
+      1
+      iex> Fibz.compute(3)
+      "fizz"
+      iex> Fibz.compute(5)
+      "buzz"
+      iex> Fibz.compute(15)
+      "fizzbuzz"
+  """
+  defdelegate compute(int), to: Fibz.Server
+
+  @doc ~S"""
+  Stops the server, normally.
+
+  ## Example
+
+      iex> Application.start(Fibz, [])
+      ...> Fibz.stop_compute_server
+      :ok
+  """
+  defdelegate stop_compute_server, to: Fibz.Server, as: :stop
 end
